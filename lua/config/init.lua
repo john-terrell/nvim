5ef014a26c3489921ddfe519ad3b28e99641f1af
John W. Terrell <john@coolpeople.io>
2025-09-14 21:30:15 -0700

    WIP

--- lua/config/init.lua
+++
@@ -1,4 +0,0 @@
--- NOTE: Lazyvim auto requires autocmds, keymaps, lazy, and options so
--- they're not needed here
-require("config.lsp")
-require("config.cmake-tools")
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,12 +1,26 @@
 return {
     "stevearc/conform.nvim",
     opts = {
-        formatters_by_ft = {
-            cpp = { "clang-format" },
+        formatters = {
+            squeeze_blanks = {
+                command = "cat"
+            }
         },
-        format_on_save = {
-            lsp_fallback = true,
-            async = false,
-        }
+        formatters_by_ft = {
+            _ = {
+                "squeeze_blanks",
+                "trim_whitespace",
+                "trim_newlines"
+            },
+            lua = { "stylua" },
+            go = { "goimports", "golines", "gofmt" },
+            cpp = { "clang-format" },
+            rust = { "rustfmt", lsp_format = "fallback" },
+        },
+    -- NOTE: The below is done already by Lazyvim
+    --    format_on_save = {
+    --        lsp_fallback = true,
+    --        async = false,
+    --    }
     }
 }
