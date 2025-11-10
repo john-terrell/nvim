01526550655936cbb051e80b5a4c471e09543005
John W. Terrell <john@alphapixel.com>
2025-11-10 11:15:46 -0800

    Updates

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Set highlight groups
+vim.api.nvim_create_autocmd("BufRead", {
+  callback = function()
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#000000", bg = "#aaaaaa" })
+    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#ffa0a0" })
+    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#a0ffa0" })
+  end,
+})
