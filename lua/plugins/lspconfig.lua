1b6bdfc780b690cdc9b2495b21e0c3e22d77431a
John W. Terrell <john@coolpeople.io>
2025-12-26 17:50:32 -0800

    Disabled mason from loading clangd since on NixOS this tries to install clangd that won't run on nix

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,18 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          mason = false,
+        }
+      },
+    },
+  },
+}
